2716e2946647ba85cd6af32fd3a93a9c8fd941bb
Danjuma Ashiwaju <25222958+danju4rizzl@users.noreply.github.com>
2019-04-16 18:09:32 +0200

    Navbar +Added custom bottons + Hero section (#1)

--- variables.js
+++ variables.js
@@ -1,10 +1,15 @@
 {
 	// Site Setup
-    "logo_url": "/img/logo.png",
-    "company_name": "We Are Damage",
-    "page_title": "Static Starter - A HTML/CSS Static site starting point",
-    "page_description": "Static Starter - A HTML/CSS Static site starting point",
-    "site_name": "Static",
-    "year": "2018",
-    "twitter": "wearedamage"
+	"logo_url": "/img/logo.png",
+	"company_name": "We Are Damage",
+	"page_title": "Static Starter - A HTML/CSS Static site starting point",
+	"page_description": "Static Starter - A HTML/CSS Static site starting point",
+	"site_name": "Code Test",
+	"year": "2019",
+	"twitter": "wearedamage",
+	"nav_link_1": "Menu Link Again",
+	"nav_link_2": "Menu Link See",
+	"nav_link_3": "Menu Link"
+
+
 }
